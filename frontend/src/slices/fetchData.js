67a1cfd85b5ab0fe3d32d06dab473b888b941427
Ilia Nikulin <wixestrees@gmail.com>
2024-12-23 17:33:43 +0300

    Update fetchData.js

--- frontend/src/slices/fetchData.js
+++ frontend/src/slices/fetchData.js
@@ -2,11 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { apiRoutes } from '../routes/routes';
 
-const fetchData = createAsyncThunk(
-  'fetchData',
+// Запрос для получения списка каналов
+export const fetchChannels = createAsyncThunk(
+  'fetchChannels',
   async (header) => {
     try {
-      const response = await axios.get(apiRoutes.dataPath(), { headers: header });
+      const response = await axios.get(apiRoutes.channelsPath(), { headers: header });
       return response.data;
     } catch (error) {
       if (!error.response) {
@@ -17,4 +18,18 @@ const fetchData = createAsyncThunk(
   }
 );
 
-export default fetchData;
+// Запрос для получения всех сообщений
+export const fetchMessages = createAsyncThunk(
+  'fetchMessages',
+  async (header) => {
+    try {
+      const response = await axios.get(apiRoutes.messagesPath(), { headers: header });
+      return response.data;
+    } catch (error) {
+      if (!error.response) {
+        throw error;
+      }
+      return Promise.reject({ status: error.response.status, data: error.response.data });
+    }
+  }
+);
