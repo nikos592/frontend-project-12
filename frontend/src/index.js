e79313558fd9dd34bd3ea2f624143493ac232279
Ilia Nikulin <wixestrees@gmail.com>
2024-12-26 12:28:23 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,10 @@
 import './assets/application.scss';
-
 import { createRoot } from 'react-dom/client';
-
-import { io } from 'socket.io-client';
-
 import init from './init.jsx';
 
 const app = async () => {
-  const socket = io();
-
   const root = createRoot(document.getElementById('chat'));
-  root.render(await init(socket));
+  root.render(await init());
 };
 
 app();
