0c09abfa95034f84339485ad494ea33335c7988d
Ilia Nikulin <wixestrees@gmail.com>
2024-12-26 15:57:47 +0300

    Update init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+// import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'; 
 
 import resources from './locales/index.js';
 import store from './slices/index.js';
@@ -13,14 +13,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthContext } from './contexts/index.jsx';
 
 const init = async () => {
-  const rollbarConfig = {
-    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
-    payload: {
-      environment: 'production',
-    },
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-  };
+  // const rollbarConfig = { 
+  //   accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  //   payload: {
+  //     environment: 'production',
+  //   },
+  //   captureUncaught: true,
+  //   captureUnhandledRejections: true,
+  // };
 
   const i18n = i18next.createInstance();
 
@@ -31,19 +31,15 @@ const init = async () => {
   });
 
   return (
-    <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <I18nextProvider i18n={i18n}>
-          <Provider store={store}>
-            <AuthProvider>
-              <ApiProvider>
-                <App />
-              </ApiProvider>
-            </AuthProvider>
-          </Provider>
-        </I18nextProvider>
-      </ErrorBoundary>
-    </RollbarProvider>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <AuthProvider>
+          <ApiProvider>
+            <App />
+          </ApiProvider>
+        </AuthProvider>
+      </Provider>
+    </I18nextProvider>
   );
 };
 
