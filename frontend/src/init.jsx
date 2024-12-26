84115a921201c250b49e7f323501e883ae7b3062
Ilia Nikulin <wixestrees@gmail.com>
2024-12-26 15:30:32 +0300

    Update init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,3 +1,4 @@
+import React, { useContext, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
@@ -36,7 +37,7 @@ const init = async () => {
         <I18nextProvider i18n={i18n}>
           <Provider store={store}>
             <AuthProvider>
-              <ApiProvider  token={token}> 
+              <ApiProvider>
                 <App />
               </ApiProvider>
             </AuthProvider>
