022b4f23863ca74bbd69b29464b34ad93d282502
nikos592 <nikos592@users.noreply.github.com>
2025-01-05 05:13:21 +0300

    fix error

--- frontend/src/components/Chat/components/Messages.jsx
+++ frontend/src/components/Chat/components/Messages.jsx
@@ -5,14 +5,26 @@ import MessagesHeader from './MessagesHeader.jsx';
 import MessagesBody from './MessagesBody.jsx';
 import MessagesForm from './MessagesForm.jsx';
 
-const Messages = ({ channel, messages }) => (
-  <Col className="p-0 h-100">
-    <div className="d-flex flex-column h-100">
-      <MessagesHeader channelName={channel.name} messagesAmount={messages.length} />
-      <MessagesBody channelId={channel.id} messages={messages} />
-      <MessagesForm channelId={channel.id} />
-    </div>
-  </Col>
-);
+const Messages = ({ channel, messages }) => {
+  if (!channel) {
+    return (
+      <Col className="p-0 h-100">
+        <div className="d-flex flex-column h-100 justify-content-center align-items-center">
+          <p>Канал не найден.</p>
+        </div>
+      </Col>
+    );
+  }
+
+  return (
+    <Col className="p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <MessagesHeader channelName={channel.name} messagesAmount={messages.length} />
+        <MessagesBody channelId={channel.id} messages={messages} />
+        <MessagesForm channelId={channel.id} />
+      </div>
+    </Col>
+  );
+};
 
 export default Messages;
