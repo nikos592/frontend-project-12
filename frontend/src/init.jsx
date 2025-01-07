e02ee679ecad928d1732dfc21d9ad928b930aaff
Ilia Nikulin <wixestrees@gmail.com>
2025-01-07 13:07:31 +0300

    Update init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
@@ -15,49 +15,63 @@ import { actions as messagesActions } from './slices/messagesSlice.js';
 import { actions as channelsActions } from './slices/channelsSlice';
 import { FilterProvider } from './providers/FilterProvider.jsx';
 
-const init = async () => {
-  const rollbarConfig = {
-    accessToken: import.meta.env.REACT_APP_ROLLBAR_TOKEN,
-    environment: import.meta.env.MODE,
-  };
+const RollbarConfig = {
+  accessToken: import.meta.env.REACT_APP_ROLLBAR_TOKEN,
+  environment: import.meta.env.MODE,
+};
 
+const SocketEventsHandler = () => {
   const socket = io();
 
-  socket.on('newMessage', (payload) => {
-    store.dispatch(messagesActions.addMessage(payload));
-  });
+  useEffect(() => {
+    socket.on('newMessage', (payload) => {
+      store.dispatch(messagesActions.addMessage(payload));
+    });
 
-  socket.on('newChannel', (payload) => {
-    store.dispatch(channelsActions.addChannel(payload));
-  });
+    socket.on('newChannel', (payload) => {
+      store.dispatch(channelsActions.addChannel(payload));
+    });
 
-  socket.on('removeChannel', (payload) => {
-    store.dispatch(channelsActions.removeChannel(payload.id));
-  });
+    socket.on('removeChannel', (payload) => {
+      store.dispatch(channelsActions.removeChannel(payload.id));
+    });
 
-  socket.on('renameChannel', (payload) => {
-    store.dispatch(
-      channelsActions.renameChannel({
-        id: payload.id,
-        changes: { name: payload.name },
-      }),
-    );
-  });
+    socket.on('renameChannel', (payload) => {
+      store.dispatch(
+        channelsActions.renameChannel({
+          id: payload.id,
+          changes: { name: payload.name },
+        })
+      );
+    });
 
-  const i18n = i18next.createInstance();
+    return () => {
+      socket.off('newMessage');
+      socket.off('newChannel');
+      socket.off('removeChannel');
+      socket.off('renameChannel');
+    };
+  }, []);
 
-  i18n.use(initReactI18next).init({
+  return null;
+};
+
+const i18n = i18next.createInstance().use(initReactI18next);
+
+const init = async () => {
+  await i18n.init({
     resources,
     lng: 'ru',
     fallbackLng: 'ru',
   });
 
   return (
-    <ErrorBoundary config={rollbarConfig}>
+    <ErrorBoundary config={RollbarConfig}>
       <Provider store={store}>
         <AuthProvider>
           <FilterProvider>
             <I18nextProvider i18n={i18n}>
+              <SocketEventsHandler />
               <ApiProvider>
                 <App />
               </ApiProvider>
